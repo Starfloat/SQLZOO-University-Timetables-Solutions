c4620617595e128171485731331edb2891720561
beatAffection <53842484+beatAffection@users.noreply.github.com>
2019-08-14 03:03:29 -0400

    Create Neeps-easy-questions-solutions.sql

---
+++ Neeps-easy-questions-solutions.sql
@@ -0,0 +1,21 @@
+/*https://sqlzoo.net/wiki/Neeps_easy_questions*/
+
+--1.
+/*Give the room id in which the event co42010.L01 takes place.
+*/
+
+--2.
+/*For each event in module co72010 show the day, the time and the place.
+*/
+
+--3.
+/*List the names of the staff who teach on module co72010.
+*/
+
+--4.
+/*Give a list of the staff and module number associated with events using room cr.132 on Wednesday, include the time each event starts.
+*/
+
+--5.
+/*Give a list of the student groups which take modules with the word 'Database' in the name.
+*/
