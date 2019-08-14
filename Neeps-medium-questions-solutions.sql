ac555f9c9e78175a361efefca849f245d9c7151b
beatAffection <53842484+beatAffection@users.noreply.github.com>
2019-08-14 05:59:11 -0400

    Update Neeps-medium-questions-solutions.sql

--- Neeps-medium-questions-solutions.sql
+++ Neeps-medium-questions-solutions.sql
@@ -3,14 +3,34 @@
 --6.
 /*Show the 'size' of each of the co72010 events. Size is the total number of students attending each event.
 */
+SELECT event.id, SUM(student.sze)
+FROM student 
+JOIN attends ON (student.id=attends.student)
+JOIN event   ON (attends.event=event.id)
+WHERE event.modle = 'co72010'
+GROUP by event.id
 
 --7.
 /*For each post-graduate module, show the size of the teaching team. (post graduate modules start with the code co7).
 */
+SELECT COUNT(DISTINCT staff.id) AS 'Size of Teaching Team', modle.name
+FROM staff
+JOIN teaches ON (staff.id=teaches.staff)
+JOIN event   ON (teaches.event=event.id)
+JOIN modle   ON (event.modle=modle.id)
+WHERE modle.id LIKE '%co7%'
+GROUP BY 'Size of Teaching Team', modle.name
 
 --8.
 /*Give the full name of those modules which include events taught for fewer than 10 weeks.
 */
+SELECT DISTINCT modle.name
+FROM event
+JOIN modle  ON  (event.modle=modle.id)
+JOIN occurs ON (event.id=occurs.event)
+JOIN week   ON (occurs.week=week.id)
+GROUP BY event.id, modle.name
+HAVING COUNT(occurs.week) < 10
 
 --9.
 /*Identify those events which start at the same time as one of the co72010 lectures.
