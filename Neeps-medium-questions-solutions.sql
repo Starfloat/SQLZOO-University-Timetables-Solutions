b23a11ffc6a8f7861916ea1113f45cebd8506f41
beatAffection <53842484+beatAffection@users.noreply.github.com>
2019-08-14 04:58:56 -0400

    Create Neeps-medium-questions-solutions.sql

---
+++ Neeps-medium-questions-solutions.sql
@@ -0,0 +1,21 @@
+/*https://sqlzoo.net/wiki/Neeps_medium_questions*/
+
+--6.
+/*Show the 'size' of each of the co72010 events. Size is the total number of students attending each event.
+*/
+
+--7.
+/*For each post-graduate module, show the size of the teaching team. (post graduate modules start with the code co7).
+*/
+
+--8.
+/*Give the full name of those modules which include events taught for fewer than 10 weeks.
+*/
+
+--9.
+/*Identify those events which start at the same time as one of the co72010 lectures.
+*/
+
+--10.
+/*How many members of staff have contact time which is greater than the average?
+*/
